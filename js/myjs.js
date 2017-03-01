f665ef2c1696809da1bfa25261cc8b96c364c6fd
SAlejandroHP <saak.hg.pv@gmail.com>
2017-02-28 23:44:39 -0600

    Alejandro Livet
    Agregando el primer commit de livet, agregando los archivos para el funcionamiento de la pagina y pruebas para movil

---
+++ js/myjs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+  $(".button-collapse").sideNav();
+  $('.scrollspy').scrollSpy();
+  $('.parallax').parallax();
+  $('.carousel').carousel();
+  $('.slider').slider({
+    indicators:false
+  });
+  $('.modal').modal();
+
+
+
+});
